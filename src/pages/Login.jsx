6e46f66e21d4d2d6bc193cf8e4e4dc306d986fd2
NisamMohd <nisammohd514@gmail.com>
2026-06-20 18:30:02 +0530

    login page sturcture created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [passwd, setPasswd] = useState("");
+  const [error, setError] = useState("");
   return (
     <div>
-        
+      <div>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="example@email.com"
+        />
+        <input
+          type="password"
+          value={Passwd}
+          onChange={(e) => setPasswd(e.target.value)}
+          placeholder="Password"
+        />
+        <div>
+          <p>{error}</p>
+        </div>
+        <button>Register</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
