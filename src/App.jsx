4e91812add4d53de578e19520772d349928afb68
NisamMohd <nisammohd514@gmail.com>
2026-06-20 19:26:37 +0530

    styles added to login page. App.jsx called login page for test purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
-      <Home/>
+      <Login/>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,24 +5,30 @@ function Login() {
   const [passwd, setPasswd] = useState("");
   const [error, setError] = useState("");
   return (
-    <div>
-      <div>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="example@email.com"
-        />
-        <input
-          type="password"
-          value={Passwd}
-          onChange={(e) => setPasswd(e.target.value)}
-          placeholder="Password"
-        />
-        <div>
-          <p>{error}</p>
+    <div className="fixed inset-0 flex justify-center items-center bg-black/50">
+      <div className="bg-white p-8 rounded-lg shadow-xl w-96">
+        <h1 className="text-2xl fond-bold mb-6 text-center">Login</h1>
+        <div className="flex flex-col gap-4">
+          <input
+            className="border border-grey-400 rounded pl-2"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="example@email.com"
+          />
+          <input
+            className="border border-grey-400 rounded pl-2"
+            type="password"
+            value={passwd}
+            onChange={(e) => setPasswd(e.target.value)}
+            placeholder="Password"
+          />
+
+        {error && (
+          <p className="text-red-500 text-sm">{error}</p>
+        )}
+          <button className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Register</button>
         </div>
-        <button>Register</button>
       </div>
     </div>
   );
