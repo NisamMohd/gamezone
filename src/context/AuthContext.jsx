f8b04bec6725faab182c1fe4a24d6dfd9239dfd3
NisamMohd <nisammohd514@gmail.com>
2026-06-21 17:32:13 +0530

    authentication context created

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { createContext } from "react";
+
+
+const AuthContext = createContext()
+
+export function AuthProvider({ children }){
+    async function login(credentials){
+        try{
+            const res = await axios.get(
+                `http://localhost:3001/users?email=${credentials.email}&password=${credentials.passwd}`
+            )
+
+            const data = res.data
+
+            if(data.length > 0){
+                const loggedUser = data[0]
+            }
+        }
+    }
+}
