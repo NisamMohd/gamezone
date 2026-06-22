382d8149d419d2aeea582cd2a9fa77b0f8ce30cc
NisamMohd <nisammohd514@gmail.com>
2026-06-23 00:43:37 +0530

    css update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/svg-logos.png";
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="absolute top-0 left-0 w-full z-10 bg-white/10 backdrop-blur-md">
       <div className="flex justify-between items-center h-[15vh] px-4">
@@ -12,17 +12,39 @@ function Navbar() {
         </div>
 
         <div className="flex items-center gap-6 text-black">
-          <a href="" className="hover:text-amber-600">HOME</a>
-          <a href="" className="hover:text-amber-600">ABOUT</a>
-          <a href="" className="hover:text-amber-600">SERVICES</a>
-          <a href="" className="hover:text-amber-600">CONTACT</a>
+          <a
+            href=""
+            className=" hover:[text-shadow:0_0_8px_rgba(217,119,6,1),0_0_8px_rgba(217,119,6,1)]"
+          >
+            HOME
+          </a>
+          <a
+            href=""
+            className="hover:[text-shadow:0_0_8px_rgba(217,119,6,1),0_0_8px_rgba(217,119,6,1)]"
+          >
+            ABOUT
+          </a>
+          <a
+            href=""
+            className="hover:[text-shadow:0_0_8px_rgba(217,119,6,1),0_0_8px_rgba(217,119,6,1)]"
+          >
+            SERVICES
+          </a>
+          <a
+            href=""
+            className="hover:[text-shadow:0_0_8px_rgba(217,119,6,1),0_0_8px_rgba(217,119,6,1)]"
+          >
+            CONTACT
+          </a>
         </div>
 
         <div>
-          <button onClick={() => navigate("/login")} className="px-4 py-2 bg-blue-500 text-white rounded">
+          <button
+            onClick={() => navigate("/login")}
+            className="px-4 py-2 bg-blue-500 text-white rounded"
+          >
             Login
           </button>
-          
         </div>
       </div>
     </div>
