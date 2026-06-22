dc7a8b46cd86e50f3e942ef85b8d8e60d4436301
NisamMohd <nisammohd514@gmail.com>
2026-06-22 18:28:29 +0530

    new image added, added a transition effect to to the newly added image[yitilenew.png]

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import home1 from "../assets/hero.png";
-
+import titleWhite from "../assets/titlenew.png";
 
 function Hero() {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setAnimate(true), 100);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className='flex-1 min-h-screen w-full'>
-        <img src={home1} alt="image1" style={{height:"100vh"}} className='w-full object-cover' />
+    <div className='relative flex-1 min-h-screen w-full overflow-hidden'>
+      <img
+        src={home1}
+        alt="image1"
+        style={{ height: "100vh" }}
+        className='w-full object-cover'
+      />
+
+      <img
+        src={titleWhite}
+        alt="title"
+        className={`absolute top-1/2 left-1/2 -translate-y-1/2 transition-all duration-1000 ease-out
+          ${animate ? "translate-x-[-50%] opacity-100" : "translate-x-[-150%] opacity-0"}`}
+      />
     </div>
   )
 }
