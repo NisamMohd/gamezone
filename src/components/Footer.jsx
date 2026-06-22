7d710bc8a216f0e51f80f32817225f584f9610f5
NisamMohd <nisammohd514@gmail.com>
2026-06-23 01:44:22 +0530

    footer structure created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div></div>
+    <div>
+      <div>
+        <h1>GameZone.Pvt</h1>
+        <div>
+          <p>Email:gamezone@gmail.com</p>
+          <p>Contact: +919966558810</p>
+          <p>Location: Mumbai</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
