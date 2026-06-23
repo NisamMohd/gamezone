657a3e588478c1ca39af2b5d315da74b8992991c
NisamMohd <nisammohd514@gmail.com>
2026-06-23 12:37:04 +0530

    route added for Products.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Routes,Route } from "react-router-dom";
 import Register from "./pages/Register";
+import Products from "./pages/Products";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<Products/>} />
       </Routes>
     
     </div>
