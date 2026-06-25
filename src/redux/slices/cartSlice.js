19e6ba2e29581211f8e6e1f14da909d238ec029a
NisamMohd <nisammohd514@gmail.com>
2026-06-25 17:34:35 +0530

    store and slice created

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+
+  initialState: {
+    items: [],
+  },
+
+  reducers: {
+    setCart(state, action) {
+      state.items = action.payload;
+    },
+
+    addToCart(state, action) {
+      const existing = state.items.find(
+        item => item.id === action.payload.id
+      );
+
+      if (existing) {
+        existing.quantity += 1;
+      } else {
+        state.items.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
+    },
+
+    removeFromCart(state, action) {
+      state.items = state.items.filter(
+        item => item.id !== action.payload
+      );
+    },
+
+    clearCart(state) {
+      state.items = [];
+    },
+  },
+});
+
+export const {
+  setCart,
+  addToCart,
+  removeFromCart,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
