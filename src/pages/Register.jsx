439e5902ddb107ead30b3ce80ef78c4014630cfa
NisamMohd <nisammohd514@gmail.com>
2026-06-26 15:00:08 +0530

    css update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { registerApi } from "../services/users";
 import Login from "./Login";
 
-
 function Register() {
   const [formData, setFormData] = useState({
     name: "",
@@ -10,7 +9,7 @@ function Register() {
     passwd: "",
     cpasswd: "",
   });
-  const [error, setError] = useState('')
+  const [error, setError] = useState("");
 
   const handleChange = (e) => {
     setFormData({
@@ -19,57 +18,61 @@ function Register() {
     });
   };
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     const hasEmptyField = Object.values(formData).some(
-      (value) => value.trim() === ""
+      (value) => value.trim() === "",
     );
 
-    if(hasEmptyField){
+    if (hasEmptyField) {
       setError("All fields are required");
       return;
     }
-  }
+  };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-      <input
-        className="border border-gray-400 rounded pl-2"
-        type="text"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-        placeholder="Name"
-      />
+    <div className="fixed inset-0 flex justify-center items-center bg-black/50">
+      <div className="bg-white p-8 rounded-lg shadow-xl w-96">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+          <input
+            className="border border-gray-400 rounded pl-2"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Name"
+          />
 
-      <input
-        className="border border-gray-400 rounded pl-2"
-        type="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        placeholder="example@email.com"
-      />
+          <input
+            className="border border-gray-400 rounded pl-2"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="example@email.com"
+          />
 
-      <input
-        className="border border-gray-400 rounded pl-2"
-        type="password"
-        name="passwd"
-        value={formData.passwd}
-        onChange={handleChange}
-        placeholder="Password"
-      />
+          <input
+            className="border border-gray-400 rounded pl-2"
+            type="password"
+            name="passwd"
+            value={formData.passwd}
+            onChange={handleChange}
+            placeholder="Password"
+          />
 
-      <input
-        className="border border-gray-400 rounded pl-2"
-        type="password"
-        name="cpasswd"
-        value={formData.cpasswd}
-        onChange={handleChange}
-        placeholder="Confirm Password"
-      />
-      <p>{error}</p>
-      <button type="submit">Submit</button>
-    </form>
+          <input
+            className="border border-gray-400 rounded pl-2"
+            type="password"
+            name="cpasswd"
+            value={formData.cpasswd}
+            onChange={handleChange}
+            placeholder="Confirm Password"
+          />
+          <p>{error}</p>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
