228b8b35e40605ce9592fee9c7679d1f299d27c4
NisamMohd <nisammohd514@gmail.com>
2026-06-30 18:24:14 +0530

    registration funtions added ,context features added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import { registerApi } from "../services/users";
-import Login from "./Login";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     passwd: "",
-    cpasswd: "",
   });
-  const [error, setError] = useState("");
+
+  const [cpasswd, setCpasswd] = useState('')
+
+  const { register, error, setError } = useAuth();
+  const navigate = useNavigate()
 
   const handleChange = (e) => {
     setFormData({
@@ -18,20 +21,44 @@ function Register() {
     });
   };
 
-  const handleSubmit = (e) => {
+  async function handleSubmit(e) {
+    e.preventDefault();
     const hasEmptyField = Object.values(formData).some(
       (value) => value.trim() === "",
     );
 
     if (hasEmptyField) {
       setError("All fields are required");
+      return
+    }
+    if (formData.passwd.length < 8) {
+      setError("password must be 8 or more charectors");
+      setFormData.passwd("");
       return;
     }
-  };
+
+    if(formData.passwd !== cpasswd){
+      setError("passwords didn't match")
+      return;
+    }
+
+
+    const res = await register(formData);
+    if (!res.success) {
+      setFormData({
+        name: "",
+        email: "",
+        passwd: "",
+      });
+      return;
+    }
+    navigate('/')
+  }
 
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-black/50">
       <div className="bg-white p-8 rounded-lg shadow-xl w-96">
+        <h1 className="text-2xl fond-bold mb-6 text-center">Register</h1>
         <form onSubmit={handleSubmit} className="flex flex-col gap-2">
           <input
             className="border border-gray-400 rounded pl-2"
@@ -64,12 +91,19 @@ function Register() {
             className="border border-gray-400 rounded pl-2"
             type="password"
             name="cpasswd"
-            value={formData.cpasswd}
-            onChange={handleChange}
+            value={cpasswd}
+            onChange={(e) => setCpasswd(e.target.value)}
             placeholder="Confirm Password"
           />
-          <p>{error}</p>
-          <button type="submit">Submit</button>
+          <p className="text-red-600 text-center">{error}</p>
+          <div className="flex justify-center items-center">
+            <button
+              type="submit"
+              className="bg-blue-600 w-20 text-white rounded-2xl"
+            >
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </div>
